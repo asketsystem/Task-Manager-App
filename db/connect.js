e6f5eded40a20407015c2ee16cbfd336dc663b0e
asketsystem <asketsystem1@gmail.com>
2021-11-26 20:02:59 +0000

    Refactoring connectDB

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
-
 const connectString = 'mongodb+srv://tasks:tasks@cluster0.w7zsm.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
 
 
-mongoose
-.connect(connectString)
-.then(() =>console.log('CONNECTED TO THE DB...'))
-.catch((err) => console.log(err))
+const connectDB = (url) =>{
+    return mongoose.connect(connectString)
+    .then(() =>console.log('CONNECTED TO THE DB...'))
+    .catch((err) => console.log(err))
+
+}
+
+module.exports = connectDB
