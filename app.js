7a177ed0771b22717ed5ca7c65c536fc78477049
asketsystem <asketsystem1@gmail.com>
2021-11-28 11:06:06 +0000

    Setting Up PORT

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(errorHandlerMiddleware)
 //app.patch('/api/v1/tasks/:id')     - update task
 //app.patch('/api/v1/tasks/:id')     - delete task
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async () =>{
     try {
